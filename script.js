c44a05259ec35a7004680855711b46e72763e96d
Marco de boer <mdboer1990@gmail.com>
2021-05-16 23:44:48 +0200

    De Functies voor de opdracht

---
+++ script.js
@@ -0,0 +1,18 @@
+/** @format */
+
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions();
+
+const fivePlusSeven = () => 5 + 7;
+
+console.log(fivePlusSeven());
+
+const addFive = (a, b) => a + b;
+
+console.log(addFive(1, 4));
+
+const createObject = () => ({ greeting: "hoi" });
+
+console.log(createObject());
